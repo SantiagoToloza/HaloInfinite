5d83325c8822635da40a44dfaeba0fcad960bb05
Santi <73451747+SantiagoToloza@users.noreply.github.com>
2022-03-15 17:52:12 -0300

    ya esta hecho todo el header  y algunas correciones del nav

--- js/main.js
+++ js/main.js
@@ -11,4 +11,33 @@ bShowMobileLinks.addEventListener('click',e =>{
 moreOptions.addEventListener('click', e =>{
     e.preventDefault();
     moreMenu.classList.toggle("show");
-})
+});
+
+const videos = [
+{
+    id:`b4doITNi2RE`
+},
+{
+    id:`u7oRIRn3mc4`
+},
+{
+    id:`WOqT6U7ZtBc`
+}
+];
+
+const SilderContainer = document.querySelector('#slider');
+const currentContainer = document.querySelector('#current');
+const videoContainer = document.querySelector('#videos-container');
+const bNext =document.querySelector('.next')
+const bPrev =document.querySelector('.prev')
+
+
+let current = 0;
+
+bNext.addEventListener('click',(e)=>{})
+bPrev.addEventListener('click',(e)=>{})
+
+function renderCurrentVideo(id){
+
+    currentContainer.innerHTML= `<iframe width="100%" height="522" src="https://www.youtube.com/mbed/${id}" title="YouTube video player" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`e
+}
